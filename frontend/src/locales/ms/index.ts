53e81ddbba6270b3133d0cb95641aa179546f00f
heinazhik <heinazhikweymong@outlook.com>
2025-10-09 09:54:08 +0800

    phase 2.9

--- frontend/src/locales/ms/index.ts
+++ frontend/src/locales/ms/index.ts
@@ -2,12 +2,14 @@ import common from './common.json'
 import navigation from './navigation.json'
 import forms from './forms.json'
 import errors from './errors.json'
+import home from './home.json'
 
 const messages = {
   common,
   navigation,
   forms,
-  errors
+  errors,
+  home
 }
 
 export default messages
